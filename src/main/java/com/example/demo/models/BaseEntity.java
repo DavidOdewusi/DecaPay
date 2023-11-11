f47d50bb970ab69382fcc9b787b12b3b6af71309
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-11 22:39:41 +0100

    Added changes

--- src/main/java/com/example/demo/models/BaseEntity.java
+++ src/main/java/com/example/demo/models/BaseEntity.java
@@ -1,18 +1,18 @@
 package com.example.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
 
 
 
